ba3d96c9dc04a81d2261a6a41098248ff503afab
tuzolanacalueto <tuzolanacalueto@gmail.com>
2024-08-22 17:57:55 +0100

    chore: adjust header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../styles/Header.css';
 import logo from '../assets/logo.png';
+import { FaSearch } from 'react-icons/fa';
 
 function Header() {
     return (
         <div className="header">
             <img src={logo} alt="CoreNotes Logo" />
             <h4>CoreNotes</h4>
-            <input type="text" placeholder="Pesquisar notas..." />
+            <div className='search'>
+                <input type="text" placeholder="Pesquisar notas..." />
+                <FaSearch className='faSearch' />
+            </div>
+
         </div>
     );
 }
