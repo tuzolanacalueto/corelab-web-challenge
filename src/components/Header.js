17cb8dd3f206ffcc9b31d183dfacebe35ad95c0c
tuzolanacalueto <tuzolanacalueto@gmail.com>
2024-08-23 14:12:29 +0100

    chore: add metodo pesquisa

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,19 @@ import '../styles/Header.css';
 import logo from '../assets/logo.png';
 import { FaSearch } from 'react-icons/fa';
 
-function Header() {
+function Header({ setSearchTerm }) {
     return (
         <div className="header">
             <img src={logo} alt="CoreNotes Logo" />
             <h4>CoreNotes</h4>
             <div className='search'>
-                <input type="text" placeholder="Pesquisar notas..." />
+                <input
+                    type="text"
+                    placeholder="Pesquisar notas..."
+                    onChange={(e) => setSearchTerm(e.target.value)} // Captura o valor do input
+                />
                 <FaSearch className='faSearch' />
             </div>
-
         </div>
     );
 }
