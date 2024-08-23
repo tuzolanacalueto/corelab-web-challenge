49621e90ad0f8eefd937ad287028590e15aa9646
tuzolanacalueto <tuzolanacalueto@gmail.com>
2024-08-23 13:42:20 +0100

    fix: remove https

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://localhost:3333/",
+  baseURL: "http://localhost:3333/",
   headers: {
     "Content-Type": "application/json",
   },
